b1956d2af955e629434f2408e9828d98a4b28bae
Nicolás Salgado <nesalgadog@gmail.com>
2022-04-27 12:09:25 -0500

    hello world update

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World ¡por fin, gonorrea!";
+		return "Hello World";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
